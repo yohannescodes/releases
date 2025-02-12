953f0b20455c1206eed90be2bf91f1197ba8a93b
Yohannes Haile <yohannesakhaile@gmail.com>
2025-02-12 16:07:16 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppFramework",
-            url: "https://github.com/yohannescodes/releases/releases/download/1.0.0/App.xcframework.zip",
+            url: "https://github.com/yohannescodes/releases/releases/download/3.0.0/App.xcframework.zip",
             checksum: "b81cd7ea92681c5a649e9a4adea573d56b188d8bcaee619334f7e0d824c9f6fe"
         ),
         .binaryTarget(
             name: "FlutterEngine",
-            url: "https://github.com/yohannescodes/releases/releases/download/1.0.0/Flutter.xcframework.zip",
+            url: "https://github.com/yohannescodes/releases/releases/download/3.0.0/Flutter.xcframework.zip",
             checksum: "af474f2ed20dba39cf12678f33bd061005a1c5c28bc306462828438fec23653c"
         )
     ]
