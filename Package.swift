7ec22912bf1cf73c637b766b26abcd12e8f59d4f
Yohannes Haile <yohannesakhaile@gmail.com>
2025-02-13 11:12:34 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,23 @@
 // swift-tools-version:5.3
+ 
 import PackageDescription
-
-let package = Package(
-    name: "releases",
+ 
+let package = Package( 
+    name: "KoaloCommunity", // Changed package name to KoaloCommunity
     platforms: [
         .iOS(.v11)
     ],
+ 
     products: [
         .library(
-            name: "releases",
-            targets: ["releases"]
+            name: "KoaloCommunity", // Changed library product name to KoaloCommunity
+            targets: ["KoaloCommunityBinary"] // Target name is updated to match
         )
     ],
+ 
     targets: [
         .binaryTarget(
-            name: "releases",
+            name: "KoaloCommunityBinary", // Changed binary target name to KoaloCommunityBinary
             url: "https://github.com/yohannescodes/releases/releases/download/4.0.0/releases.zip",
             checksum: "1ebdb770269391fbfd29abefc4e48ded0b26e34fa0f3a56a9f75a7f66f95f691"
         )
