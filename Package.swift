2a9974e9766f57ff8e3100d53f394d2e4ad456bf
Yohannes Haile <yohannesakhaile@gmail.com>
2025-02-13 10:35:51 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "releases",
-            url: "https://github.com/yohannescodes/releases/releases/download/3.0.0/releases.zip",
+            url: "https://github.com/yohannescodes/releases/releases/download/4.0.0/releases.zip",
             checksum: "1ebdb770269391fbfd29abefc4e48ded0b26e34fa0f3a56a9f75a7f66f95f691"
         )
     ]
